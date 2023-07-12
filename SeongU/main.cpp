e58171984c1d478a4e615879d06c8ea9a81b5899
SeongU0815 <blacknaco85@gmail.com>
2023-07-12 15:40:37 +0900

    20230712 renewal structure
    class
    :Actor, User, Enemy
    
    function
    :Initialize, RoundInfo

--- SeongU/main.cpp
+++ SeongU/main.cpp
@@ -1,37 +1,92 @@
-#include <iostream>
-#include <random>
-#include "player.h"
+#include<iostream>
 using namespace std;
 
-Player *initialize();
-Player *enemyState();
-void roundInfo(Player *, Player *, int);
+class Actor
+{
+protected:
+	string NAME;
+	double HP;
+	double curHp;
+	int ATK;
+	int DEF;
+	int AGI;
+public:
+void ShowState()
+	{
+		cout << this->NAME << "\n";
+		cout << "HP : " << this->curHp << "   ATK : " << this->ATK << "   DEF : "
+			 << this->DEF << "   AGi : " << this->AGI << "\n";
+	}
+
+
+};
+
+class User:public Actor
+{
+public:
+	User(double hp, int atk, int def, int agi)
+	{
+		string username;
+		cin >> username;
+		this->NAME = username;
+		this->HP = hp;
+		this->curHp = HP;
+		this->ATK = atk;
+		this->DEF = def;
+		this->AGI = agi;
+	}
+};
+
+class Enemy:public Actor
+{
+public:
+	Enemy(string enemyname, double hp, int atk, int def, int agi)
+	{
+		this->NAME = enemyname;
+		this->HP = hp;
+		this->curHp = HP;
+		this->ATK = atk;
+		this->DEF = def;
+		this->AGI = agi;
+	}
+};
+
+void Initialize(Actor* &A, Actor* &B);
+void RoundInfo(int i, Actor* &A, Actor*&B);
 
 int main()
 {
-	Player *user = initialize();
-	Player *enemy = enemyState();
-	for (int i = 0;; i++)
+	Actor *user,*enemy;
+	Initialize(user,enemy);
+	cout<<"hi";
+
+	for(int i;;i++)
 	{
-		roundInfo(user, enemy, i);
-		string key;
-		cin >> key;
+		RoundInfo(i,user,enemy);
 	}
+
 }
 
-void roundInfo(Player *user, Player *enemy, int a)
+
+
+
+
+void Initialize(Actor* &A, Actor* &B)
 {
-	cout << "------------------------------------------------------"
-		 << "\n";
-	cout << "round " << a + 1 << "\n";
-	enemy->Player::showState();
-	user->Player::showState();
-	cout << "Attack : Q"
-		 << "\n"
-		 << "Defense : W"
-		 << "\n"
-		 << "Healing : E"
-		 << "\n";
-	cout << "------------------------------------------------------"
-		 << "\n";
+	cout << "Enter your name : ";
+	A =new User(200, 20, 10, 30);
+	B =new Enemy("enemy", 1000, 30, 20, 40);
+	cout << "\nStart the battle.\n";
+}
+
+void RoundInfo(int i,Actor *&A,Actor *&B)
+{
+	cout << "--------------------------------------------------\n";
+	cout << "round " << i + 1 << "\n";
+	A->ShowState();
+	B->ShowState();
+
+	cout << "\nAttack : Q\n";
+	cout << "Defense : W\n";
+	cout << "Healing : E\n";
 }
